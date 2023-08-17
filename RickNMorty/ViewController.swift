582a752cab54f7401e9f1500694b7896c019bbac
artyom <tomas.tomson@mail.ru>
2023-08-17 20:02:46 +0300

    Initial Commit

---
+++ RickNMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickNMorty
+//
+//  Created by artyom s on 17.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
