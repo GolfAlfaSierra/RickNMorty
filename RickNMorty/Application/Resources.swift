de56098b50c576b17e6e9fa65370ef32b438505c
artyom <tomas.tomson@mail.ru>
2023-08-22 11:42:06 +0300

    add more colors

--- RickNMorty/Application/Resources.swift
+++ RickNMorty/Application/Resources.swift
@@ -10,6 +10,10 @@ import UIKit
 enum AppColors {
     static let backgoundColor = UIColor(named: "background_color")
     static let cellBackgoundColor = UIColor(named: "cell_background_color")
+    static let appDarkGrayColor = UIColor(named: "app_dark_gray")
+    static let appGreenColor = UIColor(named: "app_green")
+    static let appGray = UIColor(named: "app_gray")
+    static let appOriginIconBackground = UIColor(named: "origin_icon_background")
 }
 
 enum AppFonts {
